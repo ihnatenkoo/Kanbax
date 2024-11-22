5d54d2459c9ad6d890a8ee95ca1b0320e41f7aeb
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2024-11-22 12:54:40 +0200

    Implement task management functions in Kanbax module for starting, finishing, and retrieving task states

--- lib/kanbax.ex
+++ lib/kanbax.ex
@@ -3,16 +3,20 @@ defmodule Kanbax do
   Documentation for `Kanbax`.
   """
 
-  @doc """
-  Hello world.
+  alias Kanbax.TaskFSM
 
-  ## Examples
+  def task_start(task_id) do
+    TaskFSM.start({:via, Registry, {Kanbax.TaskRegistry, task_id}})
+  end
 
-      iex> Kanbax.hello()
-      :world
+  def task_finish(task_id) do
+    TaskFSM.finish({:via, Registry, {Kanbax.TaskRegistry, task_id}})
+  end
 
-  """
-  def hello do
-    :world
+  def task_state(task_id) do
+    case Registry.lookup(Kanbax.TaskRegistry, task_id) do
+      [{_pid, _value}] -> TaskFSM.state({:via, Registry, {Kanbax.TaskRegistry, task_id}})
+      [] -> {:error, "Task not found"}
+    end
   end
 end
